9298762010f928aa59c5eaa09f4b490e959992ff
Henry2678 <soldadohuefl@gmail.com>
2024-11-04 08:50:50 -0300

    atividade33

---
+++ atividade33.py
@@ -0,0 +1,6 @@
+lista0 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+quall = int(input("digite o numero que corresponde a lista "))
+if quall == 1:
+    list1 = lista0(range(0,8))
+    print(f"{lista1} ")
+    
